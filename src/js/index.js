e0f1e1c597e23e8eb3a1dbfdd180c5a764b327c8
filipe carvalho <filiperoc@hotmail.com>
2022-06-21 21:00:06 -0300

    cc

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ const currentActive = document.querySelector('.active');
 questions.forEach(quest => {
     quest.addEventListener('click', () => {
         const replyBefore = document.querySelector('.active')
-        const replyN = quest.attributes.id.value
-        const replyAfter = document.querySelector(`.${replyN}`)
+        const questN = quest.attributes.id.value
+        const replyAfter = document.querySelector(`.${questN}`)
         if (replyBefore != null) {
             if (replyAfter.classList.contains('active')) {
                 replyAfter.classList.remove('active')
@@ -19,7 +19,7 @@ questions.forEach(quest => {
         }
         
         const arrowBefore = document.querySelector('.fill')
-        const arrowAfter = document.querySelector(`.caret${replyN}`)
+        const arrowAfter = document.querySelector(`.caret${questN}`)
         if (arrowBefore != null) {
             if (arrowAfter.classList.contains('fill')) {
                 arrowAfter.classList.remove('fill')
